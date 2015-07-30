1d71c81c09b16b9652f83c0f43618aa09a610ef2
Buooy <Buooy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-30 07:45:59 +0000

    Tag 0.6.7
    git-svn-id: https://plugins.svn.wordpress.org/buooy-aviary-editor/tags/0.6.7@1209494 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/buooy-aviary-editor.php
@@ -0,0 +1,26 @@
+<?php
+/**
+Plugin Name:     Buooy Aviary Editor
+Plugin URI:      http://buooy.com
+Description:		Buooy Aviary Editor allows you to use the powerful Aviary editor to crop, edit and manage your images.
+Version:	0.6.6
+Author:          Aaron Lee
+Author URI:      http://buooy.com
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Gets the version number
+if(!defined('BUOOY_AVIARY_EDITOR_VERSION')){
+	define('BUOOY_AVIARY_EDITOR_VERSION', 'v0.6.6');
+}
+// Gets the modal type
+if(!defined('BUOOY_AVIARY_EDITOR_MODAL')){
+	define('BUOOY_AVIARY_EDITOR_MODAL', 'thickbox');
+}
+
+// Sets up everything
+include_once __DIR__.'/'.BUOOY_AVIARY_EDITOR_VERSION.'/init.php';
